88daed9f4a53c45f49e7320554e1baba042a400e
Ivan Mazlov <ivlord@inbox.ru>
2020-10-30 22:59:06 -0700

    sixth commit

---
+++ Practice/08/C++/Zadanie_8/Zadanie_8.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale>
+
+int Zadanie_8()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b, c;
+    char d, sp1, sp2;
+    while (true)
+    {
+        scanf("%lf%c%c%c%lf", &a, &sp1, &d, &sp1, &b); // 45 * 76
+
+        while (getchar() != '\n');
+        switch (d)
+        {
+        case '+': c = a + b; break;
+        case '-': c = a - b; break;
+        case '*': c = a * b; break;
+        case '/': c = a / b; break;
+        default:
+            printf("Операция не поддерживается\n");
+            continue;
+        }
+        printf("%lf\n", c);
+        break;
+    }
+    return 0;
+}
+
+int main() {
+    Zadanie_8();
+    return 0;
+}
