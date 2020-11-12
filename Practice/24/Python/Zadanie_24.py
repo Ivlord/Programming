5ed295bc0d9e0a17afa6207c54e724bb7901bb93
Ivan Mazlov <ivlord@inbox.ru>
2020-11-13 01:16:47 +0300

    24/Python v.2

--- Practice/24/Python/Zadanie_24.py
+++ Practice/24/Python/Zadanie_24.py
@@ -12,3 +12,18 @@ def full_write():
 
     with open("out.json", "w") as write_file:
         json.dump(out, write_file)
+
+# Данная версия будет обрабатывать json файл даже, если иды будут идти несортированно (как у Вас в файле in.json)
+def full_write2():
+
+    with open("in.json", "r") as read_file:
+        data = json.load(read_file)
+    db, out = {}, []
+    for i in data:
+        if i['userId'] not in db:
+            db[i['userId']]=0
+        elif i['completed']: db[i['userId']]+=1
+    for i in sorted(list(db)):
+        out.append({'task_completed':db[i], 'userId':i})
+    with open("out.json", "w") as write_file:
+        json.dump(out, write_file)
