58f3b593812e7202c8fd9f7e0dd2a42e456ebbf7
Ivan Mazlov <ivlord@inbox.ru>
2020-11-17 18:43:37 +0300

    Nov,17 commit 1

---
+++ Practice/21/C++/Zadanie_21/Zadanie_21.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <locale>
+using namespace std;
+
+double bmi(double weight, double heigth) {
+    return weight / heigth / heigth;
+}
+
+int print_bmi(double bmi) {
+    double zones[] = { 18.5, 25.0, 30.0, 99.0 };
+    string answers[] = { "Underweight", "Normal", "Overweight", "Normal weight" };
+
+    for (int i = 0; i <= 3; i++) {
+        if (bmi < zones[i]) {
+            cout << answers[i] << endl;
+            break;
+        }
+    }
+    return 0;
+}
+
+int Zadanie_21() {
+    double weight, heigth;
+    cin >> weight >> heigth;
+    if (cin.fail()) cout << "   ошибка ввода.\n";
+    else print_bmi(bmi(weight, heigth / 100));
+    return 0;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Zadanie_21();
+}
--- Practice/24/C++/Zadanie_24/Zadanie_24.cpp
+++ Practice/24/C++/Zadanie_24/Zadanie_24.cpp
@@ -1,11 +1,14 @@
-﻿
+﻿//
+// v.2.0
+//
+//
 #include <iostream>
 #include <string>
 #include <nlohmann/json.hpp>
 #include <fstream>
-#include <sstream> // adds: std::stringstream
+//#include <sstream> // adds: std::stringstream
 
-using json = nlohmann::json;
+using json = nlohmann::json; 
 using namespace std;
 
 
@@ -16,40 +19,40 @@ int main()
     // этот массив пришлось захардкорить на 15 int-ов, 
     // т.е. "userId">14 уже не будет работать...
 
-    int ar[] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+    int ar[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; 
+ 
+    //std::ifstream inFile;
+    //inFile.open("in.json");
+    //std::stringstream strStream;
+    //strStream << inFile.rdbuf();
+    //std::string str = strStream.str();
+    //json j = json::parse(str);
 
-    std::ifstream inFile;
-    inFile.open("in.json");
-    std::stringstream strStream;
-    strStream << inFile.rdbuf();
-    std::string str = strStream.str();
-    json j = json::parse(str);
+    std::ifstream inFile("in.json");
+    json j;
+    inFile >> j;
 
-    for (int i = 0; i < j.size(); i++) {
+    for (int i = 0; i<j.size(); i++){
         if (j[i]["completed"].get<bool>()) {
             ar[j[i]["userId"].get<int>()] += 1;
         }
     }
-
-    json j2 = {};
-
-    //j2[nullptr_t].clear();  j2[0].clear(); j.contains(i); j2.has("null"))
-    //
-    // И как убрать этот "null," в начале новой json структуры, который тулится туда при создании!??!
-    // 
+    
+    json j2 = json::array();
 
     for (int i = 0; i < sizeof(ar) / sizeof(int); i++) {
         if (ar[i]) {
-            j2[i] = json::object();
-            j2[i].push_back({ "task_completed", ar[i] });
-            j2[i].push_back({ "userId", i });
+            j2.push_back({ {"task_completed", ar[i] } , {"userId", i } }); // тут всё в порядке. без null
+            
+            // закомменченый вариант давал всё время null в начале j2. почему?
+            //j2[i] = json::object();
+            //j2[i].push_back({ "task_completed", ar[i] }); j2[i].push_back({ "userId", i });
         }
     }
-
+    
     std::cout << j2.dump(4);
 
-    std::ofstream out_file("out.json"); //std::fstream stream("out.json");
-    out_file << j2;
+    std::ofstream outFile("out.json"); //std::fstream stream("out.json");
+    outFile << j2;
     return 0;
 }
-
