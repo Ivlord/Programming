e3eb9a6d71577da7dc8c00cc1c99175f3ab0a526
Ivan Mazlov <ivlord@inbox.ru>
2020-12-23 15:25:58 +0300

    Zadanie 29 v2

--- Practice/29/C++/Zadanie_29/Zadanie_29/Zadanie_29.cpp
+++ Practice/29/C++/Zadanie_29/Zadanie_29/Zadanie_29.cpp
@@ -1,26 +1,45 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
-#include <map>
 #include <iomanip>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 
-struct Subj { string name; int mark; }; // ещё вариант
+struct Student { string name = ""; int group = 0; int mathematics;	int physics; int history; int programming; };
+bool operator< (Student& a, Student& b) {
+	return a.name < b.name;
+}
+bool operator> (Student& a, Student& b) {
+	return a.name > b.name;
+}
 
-struct Student {
-	string name = "";
-	unsigned short group = 0;
-	map <string, int> exams = { {	{"mathematics",0},
-									{"physics",0},
-									{"history",0},
-									{"programming",0}} };
-};
+template<typename T>
+ostream& prt_l(ostream& out, T data, int width, string fin = "") {
+	out << setw(width) << left << data << fin;
+	return out;
+}
+ostream& operator<< (ostream& out, Student student) {
+	out << "| ";
+	prt_l(out, student.name, 12, "| ");
+	prt_l(out, student.group, 6, "| ");
+	prt_l(out, student.mathematics, 5, "| ");
+	prt_l(out, student.physics, 5, "| ");
+	prt_l(out, student.history, 5, "| ");
+	prt_l(out, student.history, 5, "| ");
+	return out;
+}
+ostream& operator<< (ostream& out, vector<Student> student_list) {
+	for (auto student : student_list) {
+		out << student << "\n";
+	}
+	return out;
+}
 
-void Bozo_sort_ar(vector<int>& vect, bool sort_type = true) {
+template <typename T>
+void Bozo_sort_ar(vector<T>& vect, bool sort_type = true) {
+
 	srand(time(NULL));
-	int idx1 = rand() % vect.size(), idx2 = rand() % vect.size(), temporary;
+	int idx1 = rand() % vect.size(), idx2 = rand() % vect.size();
+	T temporary;
 	bool need_sorting = true;
 
 	while (need_sorting) {
@@ -31,7 +50,8 @@ void Bozo_sort_ar(vector<int>& vect, bool sort_type = true) {
 		temporary = vect[idx1]; vect[idx1] = vect[idx2]; vect[idx2] = temporary;
 
 		need_sorting = false;
-		for (int i = 1; i < vect.size(); i++) {
+		for (unsigned i = 1; i < vect.size(); i++) {
+
 			if ((sort_type && (vect[i - 1] > vect[i])) || (!sort_type && (vect[i - 1] < vect[i]))) {
 				need_sorting = true; break;
 			}
@@ -40,38 +60,27 @@ void Bozo_sort_ar(vector<int>& vect, bool sort_type = true) {
 	}
 }
 
-vector<int> BozoSort(vector<int> vect, bool sort_type = true) {
-	Bozo_sort_ar(vect, sort_type);
-	return vect;
-}
+string footer = "+-------------+-------+------+------+------+------+\n";
+string header = footer + "| Name        | Group | Math | Phys | Hist | Prog |\n" + footer;
 
-void prt(string s, int w) {
-	out << setw(w) << left << s;
-}
+int main() {
+	setlocale(LC_ALL, ""); srand(time(NULL));
 
-void operator<< (ostream& out, Student student) {
-	out << "| " << setw(10) << left << student.name << "| ";
-	out << setw(7) << left << student.group; out << "| ";
-	out << setw(7) << left << student.exams["mathematics"];	out << "| "; out << setw(7) << left << student.exams["physics"];
-	out << "| "; out << setw(7) << left << student.exams["history"]; out << "| "; out << setw(7) << left << student.exams["programming"];
-	out << "|"; out << endl;
-	out << "+-----------+-------+------+------+------+------+";	out << endl;
-}
+	vector<Student> students, assholes; // двоешники не сдавшие программирование
+	vector<string> first_names{ "Akihiko", "Carmack", "Kojima", "Persson", "Molyneux", "Meier", "Newell",
+	  "Gates", "Zukerman", "Lopez", "Dolores", "Kim", "Mazlov", "Riply", "Connor", "Norris", "Li", "Harven" };
+	string second_names = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for (short i = 0; i < 10; i++) {
+		students.push_back({ first_names[rand() % first_names.size()] + " " + second_names[rand() % second_names.size()],
+			1 + rand() % 9, 2 + rand() % 4, 2 + rand() % 4, 2 + rand() % 4, 2 + rand() % 4 });
+		if ((students[i].mathematics == 2) || (students[i].physics == 2) || (students[i].history == 2) || (students[i].programming == 2)) {
+			assholes.push_back(students[i]);
+		}
+	}
+	Bozo_sort_ar(students, true); //cout << header << students << footer << "\n"; // несортированный вывод
+	cout << header << students << footer << "\nExpulsion\n\n";				// сортированный список студентов
 
-
-
-int main()
-{
-	srand(time(0));
-	setlocale(LC_ALL, "");
-	vector<Student> man;
-	vector<int> randman;
-	vector<string> names{ "Name1 S1","Name2 S2","Name3 S3","Name4 S4","Name5 S5",
-						  "Name6 S6","Name7 S7","Name8 S8","Name9 S9","Name10 S10" };
-
-	cout << "+-----------+-------+------+------+------+------+" << endl;
-	cout << "| Name      | Group | Math | Phys | Hist | Prog |" << endl;
-	cout << "+-----------+-------+------+------+------+------+" << endl;
-
+	if (assholes.size()) cout << header << assholes[rand() % assholes.size()] << "\n" << footer << "\n";
+	else cout << "Not found\n";
 	return 0;
 }
